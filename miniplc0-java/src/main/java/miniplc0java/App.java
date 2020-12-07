37efbe835083398879b174d86fa1834a278054b6
julian141710 <jk981111@163.com>
2020-12-08 01:00:24 +0800

    debug

--- miniplc0-java/src/main/java/miniplc0java/App.java
+++ miniplc0-java/src/main/java/miniplc0java/App.java
@@ -89,7 +89,7 @@ public class App {
             } catch (Exception e) {
                 // 遇到错误不输出，直接退出
                 System.err.println(e);
-                System.exit(0);
+                System.exit(1);
                 return;
             }
             for (Token token : tokens) {
@@ -121,7 +121,7 @@ public class App {
             } catch (Exception e) {
                 // 遇到错误不输出，直接退出
                 System.err.println(e);
-                System.exit(0);
+                System.exit(1);
                 return;
             }
             for (NavmInstruction instruction : instructions) {
